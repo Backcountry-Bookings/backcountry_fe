78b032e6fa8aedf9cc16b3e6f3fb1af53060c81e
Ryan Baer <ryanjbaer@outlook.com>
2023-03-24 10:08:48 -0600

    test actions with faulty test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,8 +1,10 @@
+/// <reference types="cypress" />
+
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
   it('doesnt pass', () => {
-    cy.get(false)
+    cy.get("false")
   })
 })
