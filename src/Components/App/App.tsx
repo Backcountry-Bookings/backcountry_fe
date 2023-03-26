8b0edc576afe26592535463269fe291dc9969233
Rick Vermeil <rickv85@gmail.com>
2023-03-26 13:19:32 -0600

    Set up Details section title, img, desc, map and Navbar

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -3,11 +3,13 @@ import { Route, Routes } from 'react-router-dom';
 import Dashboard from '../Dashboard/Dashboard';
 import Results from '../Results/Results';
 import Details from '../Detail/Details';
+import Navbar from '../Navbar/Navbar';
 import './App.css';
 
 function App() {
   return (
-    <main className="App">
+    <main className="app-main">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/results" element={<Results />} />
