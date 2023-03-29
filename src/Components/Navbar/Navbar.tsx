63977fdc31b04610f574dc230078f322b23070af
Craig Weller <crg.weller@gmail.com>
2023-03-29 16:25:06 -0600

    add logo and change some css on the search bar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,13 +1,16 @@
-import './Navbar.css'
+import './Navbar.css';
 import { NavLink } from 'react-router-dom';
+import backcountryLogo from '../../Assets/backcountryLogo.png';
 
 const Navbar = () => {
   return (
     <nav className='nav-main'>
       <NavLink className='title-link' to={'/'}>
+        <img src={backcountryLogo} alt='Backcountry Logo' className='logo' />
         <h1 className='site-title'>Backcountry Bookings</h1>
       </NavLink>
     </nav>
-  )
-}
+  );
+};
+
 export default Navbar;
