2fa110fac3013fcf13d68dd09ed48d592e2eb915
Colin Ciervo <colinciervo@@gmail.com>
2023-03-29 08:32:42 -0400

    Add conditions for searching

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -19,69 +19,88 @@ import "swiper/css";
 import "swiper/css/pagination";
 
 const Dashboard = () => {
-    const [searchType, setSearchType] = useState('')
-    const [search, setSearch] = useState<string>('')
+  const [searchType, setSearchType] = useState('')
+  const [search, setSearch] = useState('')
+  const [stateError, setStateError] = useState(false)
+  const [error, setError] = useState(false)
 
-    const updateInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setSearch(e.target.value)
+  const updateInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (search !== '' && searchType !== '') {
+      setStateError(false)
+      setError(false)
     }
+    setSearch(e.target.value)
+  }
 
-    const fetchCamps = () => {
-        fetchUserData(searchType, search)
-        .then(result => {
-            console.log(result)
-        })
+  const fetchCamps = () => {
+    if (searchType === 'state_code' && search.length !== 2) {
+      setStateError(true)
     }
+    else if (searchType === '' || search === '') {
+      setError(true)
+    }
+    else { 
+      setError(false)
+      setStateError(false)
+      fetchUserData(searchType, search)
+      .then(result => {
+        console.log(result)
+      })
+     }
+    
+  }
 
 
-    return (
-        <div className="dashboard">
-            <Swiper
-                spaceBetween={30}
-                pagination={{
-                    clickable: true,
-                }}
-                modules={[Pagination, Autoplay]}
-                speed={400}
-                autoplay={{ delay: 4000 }}
-                slidesPerView={1}
-                className="mySwiper"
-            >
-                <SwiperSlide className="swiper-slide" ><img src={swiper1} alt="delicate arch" /></SwiperSlide>
-                <SwiperSlide className="swiper-slide"><img src={swiper2} alt="slot canyon in Utah" /></SwiperSlide>
-                <SwiperSlide className="swiper-slide"><img src={swiper3} alt="El Capitan in Yosemite" /></SwiperSlide>
-                <SwiperSlide className="swiper-slide"><img src={swiper4} alt="River in Yosemite" /></SwiperSlide>
-                <SwiperSlide className="swiper-slide"><img src={swiper5} alt="Waterfall in southern Utah" /></SwiperSlide>
-                <SwiperSlide className="swiper-slide"><img src={swiper6} alt="Alpine lake in Glacier NP" /></SwiperSlide>
-                <SwiperSlide className="swiper-slide"><img src={swiper7} alt="Cabin and dock in Denali NP" /></SwiperSlide>
-                <SwiperSlide className="swiper-slide"><img src={swiper8} alt="Valley in Banff National Park" /></SwiperSlide>
-                <SwiperSlide className="swiper-slide"><img src={swiper9} alt="Lake with mountain in background in Lake Clark National Park" /></SwiperSlide>
-            </Swiper>
-            <div className="search-container">
-                <form>
-                    <select value={searchType} name="search-dropdown" id="search-dropdown" onChange={event => setSearchType(event.target.value)}>
-                        <option value=''>Please select a value</option>
-                        <option value="state_code">State Code</option>
-                        <option value="q">Campground Name</option>
-                        <option value="park_name">National Park</option>
-                    </select>
-                    <input
-                        type="text"
-                        placeholder="Search by name, city or zip code"
-                        className="search"
-                        onChange={updateInput}
-                    ></input>
-                </form>
-                <br />
-                <button className="search-button" onClick={fetchCamps}>Search</button>
-            </div>
-            <br />
-            <div>
-                <h2 className="fav-campgrounds-title">Your Favorite Campgrounds</h2>
-                <img className="campfire" src={campfire} alt="A campfire"></img>
-            </div>
-        </div>
-    );
+  return (
+    <div className="dashboard">
+      <Swiper
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination, Autoplay]}
+        speed={400}
+        autoplay={{ delay: 4000 }}
+        slidesPerView={1}
+        className="mySwiper"
+      >
+        <SwiperSlide className="swiper-slide" ><img src={swiper1} alt="delicate arch" /></SwiperSlide>
+        <SwiperSlide className="swiper-slide"><img src={swiper2} alt="slot canyon in Utah" /></SwiperSlide>
+        <SwiperSlide className="swiper-slide"><img src={swiper3} alt="El Capitan in Yosemite" /></SwiperSlide>
+        <SwiperSlide className="swiper-slide"><img src={swiper4} alt="River in Yosemite" /></SwiperSlide>
+        <SwiperSlide className="swiper-slide"><img src={swiper5} alt="Waterfall in southern Utah" /></SwiperSlide>
+        <SwiperSlide className="swiper-slide"><img src={swiper6} alt="Alpine lake in Glacier NP" /></SwiperSlide>
+        <SwiperSlide className="swiper-slide"><img src={swiper7} alt="Cabin and dock in Denali NP" /></SwiperSlide>
+        <SwiperSlide className="swiper-slide"><img src={swiper8} alt="Valley in Banff National Park" /></SwiperSlide>
+        <SwiperSlide className="swiper-slide"><img src={swiper9} alt="Lake with mountain in background in Lake Clark National Park" /></SwiperSlide>
+      </Swiper>
+      <div className="search-container">
+        <form>
+          <select value={searchType} name="search-dropdown" id="search-dropdown" onChange={event => setSearchType(event.target.value)}>
+            <option value=''>Please select a value</option>
+            <option value="state_code">State Code</option>
+            <option value="q">Campground Name</option>
+            <option value="park_name">National Park</option>
+          </select>
+          <input
+            type="text"
+            placeholder="Search by name, city or zip code"
+            className="search"
+            onChange={updateInput}
+          ></input>
+        </form>
+        {error && <p>Please select a type of search/Enter something into the search bar</p>}
+        {stateError && <p>State code should be two letters</p>}
+        <br />
+        <button className="search-button" onClick={fetchCamps}>Search</button>
+      </div>
+      <br />
+      <div>
+        <h2 className="fav-campgrounds-title">Your Favorite Campgrounds</h2>
+        <img className="campfire" src={campfire} alt="A campfire"></img>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
