e0798f3745b581c6cc9a7adb054e486153e0f676
Craig Weller <crg.weller@gmail.com>
2023-04-02 13:49:14 -0600

    fix bug in use effect

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "../Dashboard/Dashboard";
 import Results from "../Results/Results";
@@ -10,6 +10,7 @@ function App() {
   const [searchResults, setSearchResults] = useState({ data: [] });
   const [selectedCampground, setSelectedCampground] = useState('');
   const [favoriteCamps, setFavoriteCamps] = useState([]);
+
 
   return (
     <main className="app-main">
